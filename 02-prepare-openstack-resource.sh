f037d85f66c4ef858dfbaf5fa78112df21d4d72d
DaeSeong Kim <powerds0111@gmail.com>
2019-07-16 03:27:56 +0000

    add env

--- 02-prepare-openstack-resource.sh
+++ 02-prepare-openstack-resource.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+export LC_ALL="en_US.UTF-8"
+export OS_PROJECT_DOMAIN_NAME=default
+export OS_USER_DOMAIN_NAME=default
+export OS_PROJECT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=password
+export OS_AUTH_URL=http://keystone.openstack.svc.cluster.local:80/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2
 
 # openstack security group
 openstack security group create clusterapi
@@ -9,12 +18,10 @@ openstack security group rule create --ingress --protocol tcp --dst-port 3000:32
 openstack security group rule create --ingress --protocol tcp --dst-port 443 clusterapi
 openstack security group rule create --egress clusterapi
 
-
 # CentOS image upload
 cd ~/
 wget https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud-1905.qcow2
 openstack image create 'CentOS-7-1905' --disk-format qcow2 --file ~/CentOS-7-x86_64-GenericCloud-1905.qcow2 --container-format bare --public
-
 
 # create 2 floating ips
 openstack floating ip create public-net
