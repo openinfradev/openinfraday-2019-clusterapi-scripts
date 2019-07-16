e9ff228737679c44f1c7776c6bc431d4c968149a
sjuuun <osjuny11@gmail.com>
2019-07-16 15:38:36 +0900

    fix ssh commands

--- 05-check-pivot.sh
+++ 05-check-pivot.sh
@@ -4,8 +4,10 @@ echo "check openstack VMs"
 openstack server list
 
 echo "check pivot"
+echo "check kubernetes namespaces"
 MASTER_VM_IP=$(openstack server list | grep master | awk '{print $9}')
 ssh centos@${MASTER_VM_IP} -i ~/.ssh/openstack_tmp
+ssh -i ~/.ssh/openstack_tmp centos@${MASTER_VM_IP} -t "sudo kubectl get namespaces"
 
-sudo kubectl get namespaces
-sudo kubectl get pods -n openstack-provider-system
+echo "check clusterapi-controller pod"
+ssh -i ~/.ssh/openstack_tmp centos@${MASTER_VM_IP} -t "sudo kubectl get pods -n openstack-provider-system"
--- 06-check-clusterapi.sh
+++ 06-check-clusterapi.sh
@@ -7,5 +7,4 @@ openstack server delete ${WORKER}
 echo "log of clusterapi-controllers"
 MASTER_VM_IP=$(openstack server list | grep master | awk '{print $9}')
 ssh centos@${MASTER_VM_IP} -i ~/.ssh/openstack_tmp
-
-sudo kubectl logs -f clusterapi-controllers-0 -n openstack-provider-system
+ssh -i ~/.ssh/openstack_tmp centos@${MASTER_VM_IP} -t "sudo kubectl logs -f clusterapi-controllers-0 -n openstack-provider-system"
