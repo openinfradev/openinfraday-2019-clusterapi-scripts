90e88f80c4d7ddee688e52c2d489c953dde20f3e
sjuuun <osjuny11@gmail.com>
2019-07-16 15:42:26 +0900

    fix typo

--- 05-check-pivot.sh
+++ 05-check-pivot.sh
@@ -6,7 +6,6 @@ openstack server list
 echo "check pivot"
 echo "check kubernetes namespaces"
 MASTER_VM_IP=$(openstack server list | grep master | awk '{print $9}')
-ssh centos@${MASTER_VM_IP} -i ~/.ssh/openstack_tmp
 ssh -i ~/.ssh/openstack_tmp centos@${MASTER_VM_IP} -t "sudo kubectl get namespaces"
 
 echo "check clusterapi-controller pod"
