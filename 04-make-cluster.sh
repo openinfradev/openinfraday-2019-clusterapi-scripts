f2501f9a5161dc8912b82abb24bbd8e52980c139
DaeSeong Kim <daeseong.kim@sk.com>
2019-07-17 02:27:54 +0000

    source bashrc

--- 04-make-cluster.sh
+++ 04-make-cluster.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+source ~/.bashrc
+
 # create k8s cluster on openstack
 clusterctl create cluster --bootstrap-type kind --provider openstack -c ~/cluster.yaml -m ~/machines.yaml -p ~/provider-components.yaml
